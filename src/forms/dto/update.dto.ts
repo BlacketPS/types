ba0c2ee3595a6dda00683ae22c21ccb205c3bebf
XOTlC <twilightgeometrydash@gmail.com>
2025-03-25 02:56:44 +0000

    remove forms entirely

--- src/forms/dto/update.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, Validate } from "class-validator";
-
-export class FormsUpdateDto {
-    @IsNotEmpty()
-    @Validate((value: string) => value.length > 0)
-    readonly reasonToPlay: string;
-}
-
-export default FormsUpdateDto;
